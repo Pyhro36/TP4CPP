ea53a9e329c84ead8571d5633e33767ca6e7235c
Nicolas <nicolas.six@pau.fr>
2016-02-03 20:05:50 +0100

    addition of bash script to do tests and of one example test

---
+++ tests/test.sh
@@ -0,0 +1,27 @@
+EXE=../shape_Handler
+
+inputList=$(ls testFile/*.in)
+outputList=$(ls testFile/*.out)
+
+NumberOfInput=$( echo $inputList | wc -w)
+
+for i in `seq 1 $NumberOfInput`
+do
+	INPUT=$( echo $inputList | awk "{ print \$$i }")
+	REF_OUTPUT=$( echo $outputList | awk "{ print \$$i }")
+	OUTPUT=testFile/output/out$i
+
+	echo ""
+	echo "======================================="
+	echo "Éxecution de $INPUT"
+	echo "======================================="
+	echo ""
+
+	/usr/bin/time -v $EXE < $INPUT > $OUTPUT
+
+	echo ""
+	echo "Diférences trouvées avec la sortie attendu:"
+	echo ""
+
+	diff $OUTPUT $REF_OUTPUT --report-identical-files --side-by-side --suppress-common-lines
+done
